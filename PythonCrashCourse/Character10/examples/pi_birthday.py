62cdfcbc0a21caac2bc2ae837172a27499d63a1d
Yejing Huang <huang_yejing@126.com>
2017-12-27 17:15:49 +0800

    10.1

---
+++ PythonCrashCourse/Character10/examples/pi_birthday.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  pi_birthday.py
+#  
+#  Copyright 2017 c5220056 <c5220056@GMPTIC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+filename = 'pi_million_digits.txt'
+
+with open(filename) as file_object:
+	lines = file_object.readlines()
+	
+pi_string = ''
+for line in lines:
+	pi_string += line.rstrip()
+	
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+	print("Your birthday appears in the first million digits of pi!")
+else:
+	print("Your birthday does not appear in the first million digits of pi,")
---
+++ PythonCrashCourse/Character10/exercises/10-2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  10-2.py
+#  
+#  Copyright 2017 c5220056 <c5220056@GMPTIC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+filename = 'learning_python.txt'
+
+with open(filename) as file_objects:
+	lines = file_objects.readlines()
+	
+for line in lines:
+	message = line.replace('Python', 'C')
+	print(message.rstrip())
