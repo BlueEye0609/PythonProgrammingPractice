6e498d8c9e34c240f0beddf73e0b1b8a21a79b36
Yejing Huang <huang_yejing@126.com>
2017-12-28 15:03:28 +0800

    10.3

---
+++ PythonCrashCourse/Character10/exercises/10-7.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  10-7.py
+#  
+#  Copyright 2017 c5220056 <c5220056@GMPTIC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+print("Give me two numbers, and I'll add them.")
+print("Enter 'q' to quit.")
+
+while True:
+	first_number = input("\nFirst Number: ")
+	if first_number == 'q':
+		break
+	second_number = input("Second number: ")
+	if second_number == 'q':
+		break
+	try:
+		answer = int(first_number) + int(second_number)
+	except ValueError:
+		print("Please enter numbers")
+	else:
+		print(answer)
