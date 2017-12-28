2cef6e9cc22a2a906db45d71f98d2f8b39260627
Yejing Huang <huang_yejing@126.com>
2017-12-28 16:20:26 +0800

    10-4

---
+++ PythonCrashCourse/Character10/exercises/10-11-1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  10-11-1.py
+#  
+#  Copyright 2017 c5220056 <c5220056@GMPTIC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import json
+
+filename = "favorite_number.json"
+
+favorite_number = input("Please type your favorite number and we will remember it: ")
+with open(filename, 'w') as f_obj:
+	json.dump(favorite_number, f_obj)
---
+++ PythonCrashCourse/Character10/exercises/10-13.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  10-13.py
+#  
+#  Copyright 2017 c5220056 <c5220056@GMPTIC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+import json
+
+def get_stored_username():
+	filename = 'username.json'
+	try:
+		with open(filename) as f_obj:
+			username = json.load(f_obj)
+	except FileNotFoundError:
+		return None
+	else:
+		return username
+
+def get_new_username():
+	username = input("What is your name? ")
+	filename = 'username.json'
+	with open(filename, 'w') as f_obj:
+		json.dump(username, f_obj)
+	return username
+		
+		
+def greet_user():
+	username = get_stored_username()
+	if username:
+		print("Welcome back, " + username + "!")
+	else:
+		username = get_new_username()
+		print("We'll remember you when you come back, " + username + "!")
+		
+
+def confirmed_user():
+	while True:
+		confirm = input("Please confirm if your username is right or not. y/n, type 'q' to quit: ")
+		
+		if confirm == 'q':
+			break
+		elif confirm == 'y':
+			return 1
+		elif confirm == 'n':
+			get_new_username()
+		else:
+			print("Sorry, I don't understand your typing.")
+	
+if confirmed_user():
+	greet_user()
+
+
